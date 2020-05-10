bc62f27be15babcc6b2fc7830cda6fb6a3564495
priyanshgarg62 <58046633+priyanshgarg62@users.noreply.github.com>
2020-05-10 16:09:00 +0530

    added Configuration annotation

--- src/main/java/com/amdocs/media/Cloud/Gateway/Service/BeanConfig.java
+++ src/main/java/com/amdocs/media/Cloud/Gateway/Service/BeanConfig.java
@@ -3,7 +3,9 @@ package com.amdocs.media.Cloud.Gateway.Service;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class BeanConfig {
 	
 	@Bean
